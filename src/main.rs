76b5e975839bf447f33fcacbe5c26f6601c441ef
David Sugar <foobarc11@protonmail.ch>
2020-11-18 19:32:40 +0100

    Backend server added.

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,6 @@ fn file(file: PathBuf) -> Option<NamedFile> {
 fn main() {
     rocket::ignite()
         .mount("/", routes![index, file])
-        .mount("/", StaticFiles::from("/static"))
+        .mount("/", StaticFiles::from("static"))
         .launch();
 }
